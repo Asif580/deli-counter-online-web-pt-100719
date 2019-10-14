caf2615290aff1cf95a0b48d1cb10b3d34a194a4
Asiful Choudhury <asiful307@gmail.com>
2019-10-14 20:13:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,12 @@ def line(katz_deli)
   if katz_deli.empty?
   puts "The line is currently empty."
 else current_line = "The line is currently:"
-  katz_deli.each(1) do |person, i|
+  katz_deli.each.with_index(1) do |person, i|
     current_line << " #{i}. #{person}"
   end 
   puts current_line
 end 
 end
+
+def take_a_number(katz_deli, name)
+  
